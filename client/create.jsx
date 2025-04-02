506faa1838ffd26f72e3a0764334229e51f0ed85
Mindflexdev <sales@mindflex.berlin>
2025-04-02 12:06:07 +0200

    Create create.jsx

---
+++ client/create.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Create() {
+  return (
+    <div>
+      <h2>Neue Session erstellen</h2>
+      <form>
+        <input placeholder="Titel" />
+        <button type="submit">Starten</button>
+      </form>
+    </div>
+  )
+}
